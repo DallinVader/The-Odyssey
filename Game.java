148ee6670b9a36f2f43ef2d15a93fc190e797cd0
DallinVader <dallin@riafox.com>
2024-04-19 16:18:55 -0600

    Added basic window

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public class Game{
     public static void main(String[] args){
         JFrame frame = new JFrame("The-Oddyesy");
-        frame.getContentPane().setBackground(new Color(120, 52, 124));
+        frame.getContentPane().setBackground(new Color(193,228,254));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
