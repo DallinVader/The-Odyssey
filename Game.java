d91be51143ebc3d34b476eb4cc6ff81722083a19
DallinVader <dallin@riafox.com>
2024-04-18 22:16:04 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,9 +3,9 @@ import java.awt.Color;
 
 
 public class Game{
-    public void main(String[] args){
+    public static void main(String[] args){
         JFrame frame = new JFrame("The-Oddyesy");
-        frame.getContentPane().setBackground(new Color(38, 139, 7));
+        frame.getContentPane().setBackground(new Color(120, 52, 124));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
