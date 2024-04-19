ffb9666e68c495b0d70aab3602265b995170285d
DallinVader <dallin@riafox.com>
2024-04-18 22:10:34 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,15 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+
+
 public class Game{
     public void main(String[] args){
-        System.out.print("5");
+        JFrame frame = new JFrame("The-Oddyesy");
+        frame.getContentPane().setBackground(new Color(38, 139, 7));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(1600, 900);
+        frame.setLocationRelativeTo(null);
     }
 }
